ef96c61a38039d9408d4d334393ce3ba90f8d374
umeyuki <umeyuki1326@gmail.com>
2020-11-13 13:07:02 +0900

    build webpack

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "home#index"
+  resources :home
+  resource :time_travel, only: [:create] if Rails.env.development? || Rails.env.staging?
 end
